e1dfae35f2bf7a90ceeb60af49261207bc514cd9
Raphael Chambers <raphaelchambers@gmail.com>
2025-03-10 20:47:52 +1100

    add service worker (needs https)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,16 +11,19 @@ export const Home: React.FC<HomeProps> = ({ onClick }) => {
             </p>
             <p>
                 Our platform allows you to browse artists, view detailed profiles, and
-                curate your own personalized gallery. Whether you're an art enthusiast
-                or a casual admirer, ArtGallery Explorer brings the world of art to your fingertips.
+                explore galleries.
             </p>
             <p>
-                Click on an artist to view their profile, explore their masterpieces,
-                and learn more about their creative journey. Start building your favorite collection today!
+                Whether you're an art enthusiast
+                or a casual admirer, ArtViewer brings the world of art to your fingertips.
+            </p>
+            <p>
+                Click on an artist to view their profile, explore their works,
+                and learn more about their creative journey.
             </p>
             <div className="buttons">
                 <p className="get-started" onClick={onClick}>Get Started</p>
             </div>
-        </div>
+        </div >
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.error('ServiceWorker registration failed: ', error);
+    });
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
