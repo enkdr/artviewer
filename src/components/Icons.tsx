1df222b3c6b33a49d9f34d508a43c4ef48338cae
Raphael Chambers <raphaelchambers@gmail.com>
2025-03-10 11:20:13 +1100

    fix icon attributes

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -42,7 +42,7 @@ export const Icon: React.FC<IconProps> = ({ icon, onClick, size = 24, className 
 
 
 export const defaultProfileImage = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" width="48" height="48" strokeWidth="0.75">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="48" height="48" strokeWidth="0.75">
         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
         <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
         <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
@@ -50,7 +50,7 @@ export const defaultProfileImage = (
 );
 
 export const defaultArtworkImage = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="48" height="48" stroke-width="0.75">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="48" height="48" strokeWidth="0.75">
         <path d="M15 8h.01"></path>
         <path d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z"></path>
         <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5"></path>
